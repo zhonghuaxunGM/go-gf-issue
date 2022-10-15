759f37300b56ab9a891d1a7135b794167c7b282c
Xu <zhenghao.xu>
2022-10-15 23:19:24 +0800

    issue-2105

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-gf-issue
 
 go 1.16
 
-require github.com/gogf/gf/v2 v2.2.0
+require (
+	github.com/gogf/gf/contrib/drivers/mysql/v2 v2.2.0
+	github.com/gogf/gf/v2 v2.2.1-0.20221014021027-2598745e50c9
+)
--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-	"go-gf-issue/internal/cmd"
+	"fmt"
+	"go-gf-issue/internal/dao"
 
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
+	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
 )
 
+// https://github.com/gogf/gf/pull/2213
 func main() {
-	cmd.Main.Run(gctx.New())
+	type JsonItem struct {
+		Name  string `json:"name,omitempty"`
+		Value string `json:"value,omitempty"`
+	}
+	type Test struct {
+		Id   string      `json:"id,omitempty"`
+		Json []*JsonItem `json:"json,omitempty"`
+	}
+
+	ctx := gctx.New()
+	var list []*Test
+	err := dao.Test.Ctx(ctx).Scan(&list)
+	if err != nil {
+		return
+	}
+	g.Dump(list[0])
+	fmt.Println(list[0].Json)
+	fmt.Println(len(list[0].Json))
+	g.Dump(list[1])
+	fmt.Println(list[1].Json)
+	fmt.Println(len(list[1].Json))
+	//cmd.Main.Run(gctx.New())
 }

a9c03b2bf17dd9456414091d8251bf523af7ef82
Xu <zhenghao.xu>
2022-10-16 16:32:15 +0800

    issue 2181

--- main.go
+++ main.go
@@ -7,10 +7,36 @@ import (
 	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
+	"github.com/gogf/gf/v2/os/gtime"
 )
 
-// https://github.com/gogf/gf/pull/2213
+// CREATE TABLE `test_1665845241938231900` (
+// 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+// 	`passport` varchar(45) DEFAULT NULL,
+// 	`password` char(32) DEFAULT NULL,
+// 	`nickname` varchar(45) DEFAULT NULL,
+// 	`create_time` timestamp NULL DEFAULT NULL,
+// 	PRIMARY KEY (`id`)
+//   ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+// INSERT INTO `gf_issue`.`test_1665845241938231900` (`id`, `passport`, `password`, `nickname`, `create_time`) VALUES ('11', 'user_11', 'pass_11', 'name_11', '2018-10-24 10:00:00');
+
 func main() {
+	issue2181()
+	// issue2105()
+	//cmd.Main.Run(gctx.New())
+}
+func issue2181() {
+	startTime := gtime.New(1664640000)
+	fmt.Println(startTime) // 2022-10-02 00:00:00
+
+	endTime := gtime.New(1665308819)
+	fmt.Println(endTime) //2022-10-09 17:46:59
+
+	cnt, err := dao.Test1665845241938231900.Ctx(gctx.New()).Where("create_time >= ? and create_time <= ?", startTime, endTime).Count()
+	fmt.Println(cnt, err)
+}
+
+func issue2105() {
 	type JsonItem struct {
 		Name  string `json:"name,omitempty"`
 		Value string `json:"value,omitempty"`
@@ -32,5 +58,4 @@ func main() {
 	g.Dump(list[1])
 	fmt.Println(list[1].Json)
 	fmt.Println(len(list[1].Json))
-	//cmd.Main.Run(gctx.New())
 }
